18ec076b8046c406ae2cb3a8ed2486fb5b1892da
Marcos Sombra <markos.vs@live.com>
2017-06-01 06:39:22 -0300

    criar metodo iniciar para camara facil

--- Camara.java
+++ Camara.java
@@ -14,6 +14,16 @@ public class Camara{
 
 	//métodos
 	public void iniciar(TipoCamara tipo, Aventureiro av){
-		//
+		//variavel pra armazenar o tamanho da matriz
+		int length = this.cenario.length;
+		//cria um ObjetoCamara tesouro
+		ObjetoCamara tes = new ObjetoCamara(TipoElemento.TESOURO, "Tesouro");
+		//cria um aventureiro igual ao passado como parametro e coloca na posição (0,0)
+		this.cenario[0][0] = new ObjetoCamara(av.getTipo(), av.getNome());
+		//switch-case com os tipos de camara
+		switch(tipo){
+			case FACIL:
+				this.cenario[length-1][length-1] = tes;
+		}
 	}
 }
