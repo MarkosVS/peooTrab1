fa4085f0a0d3347ecf3e6b151676ff2a20bdb78d
Marcos Sombra <markos.vs@live.com>
2017-06-01 19:55:56 -0300

    att classe main (p/ testes)

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
 	public static void main(String[] args){
 		Aventureiro mv = new Aventureiro("Marcos Vinicius");
 		Caverna cav = new Caverna(mv, 10);
+		cav.iniciar(TipoCamara.FACIL);
 	}
 }
