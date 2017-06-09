24450d89d0c21bb6b6ecb165cb00787da4938dde
Marcos Sombra <markos.vs@live.com>
2017-06-09 06:24:13 -0300

    att main (testando classes)

--- Main.java
+++ Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Main{
 	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		//menu inicial
+		System.out.println("Digite o tamanho da câmara: ");
+		int tamanho = scan.nextInt();
 		Aventureiro mv = new Aventureiro("Marcos Vinicius");
-		Caverna cav = new Caverna(mv, 10);
+		Caverna cav = new Caverna(mv, tamanho);
 		cav.iniciar(TipoCamara.FACIL);
 		cav.visualizarCenario();
+		/*
+		System.out.println("");
+		System.out.println(cav.getCamara().getCenario()[tamanho-2][tamanho-1].getTipo());
+		*/
 	}
 }
