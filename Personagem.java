a6c3cf50c236b5a47e33f539d6e86e006a546abb
Marcos Sombra <markos.vs@live.com>
2017-06-10 09:23:11 -0300

    inicio metodo mover()

--- Personagem.java
+++ Personagem.java
@@ -16,4 +16,38 @@ public abstract class Personagem extends Elemento{
 	public void setNome(String nome){
 		this.nome = nome;
 	}
+        
+        //métodos
+        public void mover(Camara cam, Movimento mov) throws AcaoInvalidaException{
+            //posição do personagem
+            int x = 0, y = 0;
+            //tamanho da camara
+            int length = cam.getCenario().length;
+            boolean flag = false; //flag para quebrar o duplo for
+            //duplo for para encontrar a posição do personagem
+            for(int i = 0; i < length && !flag; i++)
+                for(int j = 0; j < length && !flag; j++){
+                    if(cam.getCenario()[i][j].getTipo() == this.getTipo()){
+                        x = i;
+                        y = j;
+                        flag = true;
+                    }
+                }
+            
+            //switch-case pra realizar o movimento
+            switch(mov){
+                case UP:
+                    if(x == 0)
+                        throw new AcaoInvalidaException();
+                    break;
+                case DOWN:
+                    break;
+                case LEFT:
+                    break;
+                case RIGHT:
+                    break;
+                default:
+                    break;
+            }
+        }
 }
