9d9202ece3745135cb7e42489238838ac6886c97
Marcos Sombra <markos.vs@live.com>
2017-06-10 07:10:41 -0300

    att main (testes)

--- Main.java
+++ Main.java
@@ -4,15 +4,15 @@ public class Main{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		//menu inicial
-		System.out.println("Digite o tamanho da câmara: ");
+		System.out.println("Digite o tamanho da câmara: (Mínimo: 5)");
 		int tamanho = scan.nextInt();
+                while(tamanho < 5){
+                    System.out.println("Digite um valor maior ou igual a 5.");
+                    tamanho = scan.nextInt();
+                }
 		Aventureiro mv = new Aventureiro("Marcos Vinicius");
 		Caverna cav = new Caverna(mv, tamanho);
 		cav.iniciar(TipoCamara.DIFICIL);
 		cav.visualizarCenario();
-		/*
-		System.out.println("");
-		System.out.println(cav.getCamara().getCenario()[tamanho-2][tamanho-1].getTipo());
-		*/
 	}
 }
