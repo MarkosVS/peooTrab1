731189ab9988605afab1d67bf0193c20cbdfaf5f
Marcos Sombra <markos.vs@live.com>
2017-06-11 17:46:29 -0300

    atualizar metodo mover()

--- Personagem.java
+++ Personagem.java
@@ -1,21 +1,23 @@
+package trabexceção;
+
 public abstract class Personagem extends Elemento{
-    //atributos
-    private String nome;
+	//atributos
+	private String nome;
 
-    //construtor
-    public Personagem(TipoElemento t, String nome){
-        super(t);
-        this.nome = nome;
-    }
+	//construtor
+	public Personagem(TipoElemento t, String nome){
+		super(t);
+		this.nome = nome;
+	}
 
-    //getter / setter
-    public String getNome(){
-        return this.nome;
-    }
+	//getter / setter
+	public String getNome(){
+		return this.nome;
+	}
 
-    public void setNome(String nome){
-        this.nome = nome;
-    }
+	public void setNome(String nome){
+		this.nome = nome;
+	}
         
         //métodos
         public void mover(Camara cam, Movimento mov) throws AcaoInvalidaException{
@@ -46,7 +48,7 @@ public abstract class Personagem extends Elemento{
                     //não deixar o personagem atravessar um obstáculo
                     if(cam.getCenario()[x-1][y].getTipo() == TipoElemento.OBSTACULO){
                         cam.visualizarCenario();
-                        throw new AcaoInvalidaException("Não é possível se atravessar um obstáculo");
+                        throw new AcaoInvalidaException("Não é possível atravessar um obstáculo");
                     }
                     
                     //checa se acima do personagem é um espaço vazio, se sim, já realiza o movimento
@@ -72,10 +74,117 @@ public abstract class Personagem extends Elemento{
                     }
                     break;
                 case DOWN:
+                    //não deixar o personagem sair da camara
+                    if(x == length - 1){
+                        cam.visualizarCenario();
+                        throw new AcaoInvalidaException("Não é possível sair da câmara");
+                    }
+                    
+                    //não deixar o personagem atravessar um obstáculo
+                    if(cam.getCenario()[x+1][y].getTipo() == TipoElemento.OBSTACULO){
+                        cam.visualizarCenario();
+                        throw new AcaoInvalidaException("Não é possível atravessar um obstáculo");
+                    }
+                    
+                    //checa se abaixo do personagem é um espaço vazio, se sim, já realiza o movimento
+                    if(cam.getCenario()[x+1][y].getTipo() == TipoElemento.EPACOVAZIO){
+                       cam.getCenario()[x+1][y] = cam.getCenario()[x][y];
+                       cam.getCenario()[x][y] = new ObjetoCamara(TipoElemento.EPACOVAZIO, "Espaço vazio");
+                       cam.visualizarCenario();
+                       return;
+                    }
+                    
+                    //checa o tipo do personagem
+                    if(cam.getCenario()[x][y].getTipo() == TipoElemento.AVENTUREIRO){
+                        if(cam.getCenario()[x+1][y].getTipo() == TipoElemento.JOAODORMINHOCO){
+                            cam.visualizarCenario();
+                            throw new AcaoInvalidaException("Não é possível ir em direção ao João Dorminhoco");
+                        }else if(cam.getCenario()[x+1][y].getTipo() == TipoElemento.TESOURO || cam.getCenario()[x+1][y].getTipo() == TipoElemento.MENSAGEM){
+                            cam.getCenario()[x+1][y] = cam.getCenario()[x][y];
+                            cam.getCenario()[x][y] = new ObjetoCamara(TipoElemento.EPACOVAZIO, "Espaço vazio");
+                            cam.visualizarCenario();
+                        }
+                    }else if(cam.getCenario()[x][y].getTipo() == TipoElemento.JOAODORMINHOCO){
+                        if(cam.getCenario()[x+1][y].getTipo() == TipoElemento.AVENTUREIRO){
+                            cam.getCenario()[x+1][y] = cam.getCenario()[x][y];
+                            cam.getCenario()[x][y] = new ObjetoCamara(TipoElemento.EPACOVAZIO, "Espaço vazio");
+                            cam.visualizarCenario();
+                        }
+                    }
                     break;
                 case LEFT:
+                    //não deixar o personagem sair da camara
+                    if(y == 0){
+                        cam.visualizarCenario();
+                        throw new AcaoInvalidaException("Não é possível sair da câmara");
+                    }
+                    
+                    //não deixar o personagem atravessar um obstáculo
+                    if(cam.getCenario()[x][y-1].getTipo() == TipoElemento.OBSTACULO){
+                        cam.visualizarCenario();
+                        throw new AcaoInvalidaException("Não é possível atravessar um obstáculo");
+                    }
+                    
+                    //checa se à esquerda do personagem é um espaço vazio, se sim, já realiza o movimento
+                    if(cam.getCenario()[x][y-1].getTipo() == TipoElemento.EPACOVAZIO){
+                       cam.getCenario()[x][y-1] = cam.getCenario()[x][y];
+                       cam.getCenario()[x][y] = new ObjetoCamara(TipoElemento.EPACOVAZIO, "Espaço vazio");
+                       cam.visualizarCenario();
+                       return;
+                    }
+                    
+                    //checa o tipo do personagem
+                    if(cam.getCenario()[x][y].getTipo() == TipoElemento.AVENTUREIRO){
+                        if(cam.getCenario()[x][y-1].getTipo() == TipoElemento.JOAODORMINHOCO){
+                            cam.visualizarCenario();
+                            throw new AcaoInvalidaException("Não é possível ir em direção ao João Dorminhoco");
+                        }
+                    }else if(cam.getCenario()[x][y].getTipo() == TipoElemento.JOAODORMINHOCO){
+                        if(cam.getCenario()[x][y-1].getTipo() == TipoElemento.AVENTUREIRO){
+                            cam.getCenario()[x][y-1] = cam.getCenario()[x][y];
+                            cam.getCenario()[x][y] = new ObjetoCamara(TipoElemento.EPACOVAZIO, "Espaço vazio");
+                            cam.visualizarCenario();
+                        }
+                    }
                     break;
                 case RIGHT:
+                    //não deixar o personagem sair da camara
+                    if(y == length + 1){
+                        cam.visualizarCenario();
+                        throw new AcaoInvalidaException("Não é possível sair da câmara");
+                    }
+                    
+                    //não deixar o personagem atravessar um obstáculo
+                    if(cam.getCenario()[x][y+1].getTipo() == TipoElemento.OBSTACULO){
+                        cam.visualizarCenario();
+                        throw new AcaoInvalidaException("Não é possível atravessar um obstáculo");
+                    }
+                    
+                    //checa se à direita do personagem é um espaço vazio, se sim, já realiza o movimento
+                    if(cam.getCenario()[x][y+1].getTipo() == TipoElemento.EPACOVAZIO){
+                       cam.getCenario()[x][y+1] = cam.getCenario()[x][y];
+                       cam.getCenario()[x][y] = new ObjetoCamara(TipoElemento.EPACOVAZIO, "Espaço vazio");
+                       cam.visualizarCenario();
+                       return;
+                    }
+                    
+                    //checa o tipo do personagem
+                    if(cam.getCenario()[x][y].getTipo() == TipoElemento.AVENTUREIRO){
+                        if(cam.getCenario()[x][y+1].getTipo() == TipoElemento.JOAODORMINHOCO){
+                            cam.visualizarCenario();
+                            throw new AcaoInvalidaException("Não é possível ir em direção ao João Dorminhoco");
+                        }else if(cam.getCenario()[x][y+1].getTipo() == TipoElemento.TESOURO || cam.getCenario()[x+1][y].getTipo() == TipoElemento.MENSAGEM){
+                            cam.getCenario()[x][y+1] = cam.getCenario()[x][y];
+                            cam.getCenario()[x][y] = new ObjetoCamara(TipoElemento.EPACOVAZIO, "Espaço vazio");
+                            cam.visualizarCenario();
+                        }
+                    }else if(cam.getCenario()[x][y].getTipo() == TipoElemento.JOAODORMINHOCO){
+                        if(cam.getCenario()[x][y+1].getTipo() == TipoElemento.AVENTUREIRO){
+                            cam.getCenario()[x][y+1] = cam.getCenario()[x][y];
+                            cam.getCenario()[x][y] = new ObjetoCamara(TipoElemento.EPACOVAZIO, "Espaço vazio");
+                            cam.visualizarCenario();
+                        }
+                    }
                     break;
                 default:
                     break;
