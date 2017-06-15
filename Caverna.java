1a6ed4bd59ea8cd2bb668bf6af5558845c76e9cb
Marcos Sombra <markos.vs@live.com>
2017-06-15 09:09:48 -0300

    att metodo

--- Caverna.java
+++ Caverna.java
@@ -1,5 +1,7 @@
 package trabexceção;
 
+import java.util.Random;
+
 public class Caverna{
 	//atributos
 	private Camara camara;
@@ -32,6 +34,17 @@ public class Caverna{
 	//métodos
 	public void iniciar(TipoCamara tipo){
 		this.camara.iniciar(tipo, this.aventureiro);
+	}
+        
+        public void iniciar(){
+                Random jv = new Random();
+                int n = jv.nextInt(3);
+                if(n == 0)
+                    this.camara.iniciar(TipoCamara.FACIL ,this.aventureiro);
+                else if(n == 1)
+                    this.camara.iniciar(TipoCamara.DIFICIL ,this.aventureiro);
+                else
+                    this.camara.iniciar(TipoCamara.ERRADA ,this.aventureiro);
 	}
 
 	public void visualizarCenario(){
