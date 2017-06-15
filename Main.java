9820feb93db4165080ae9248193040241aa32826
Marcos Sombra <markos.vs@live.com>
2017-06-15 13:51:51 -0300

    att main (incompleta)

--- Main.java
+++ Main.java
@@ -3,18 +3,20 @@ package trabexceção;
 import java.util.Scanner;
 
 public class Main{
-	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
-		//menu inicial
-		System.out.println("Digite o tamanho da câmara: (Mínimo: 5)");
-		int tamanho = scan.nextInt();
-                while(tamanho < 5){
-                    System.out.println("Digite um valor maior ou igual a 5.");
-                    tamanho = scan.nextInt();
-                }
-		Aventureiro mv = new Aventureiro("Marcos Vinicius");
-		Caverna cav = new Caverna(mv, tamanho);
-		cav.iniciar(TipoCamara.DIFICIL);
-		cav.visualizarCenario();
-	}
+    static void jogar(Caverna cav){
+        cav.iniciar();
+    }    
+    public static void main(String[] args){
+	Scanner scan = new Scanner(System.in);
+        //menu inicial
+        System.out.println("Digite o tamanho da câmara: (Mínimo: 5)");
+        int tamanho = scan.nextInt();
+        while(tamanho < 5){
+            System.out.println("Digite um valor maior ou igual a 5.");
+            tamanho = scan.nextInt();
+        }
+        Aventureiro mv = new Aventureiro("Marcos Vinicius");
+        Caverna cav = new Caverna(mv, tamanho);
+        jogar(cav);
+    }
 }
