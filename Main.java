71b6671fab8e81c27cc23e9b6b386a6ff69dd43f
Marcos Sombra <markos.vs@live.com>
2017-06-15 15:15:40 -0300

    metodo jogar quase concluido para camara errada

--- Main.java
+++ Main.java
@@ -1,22 +1,63 @@
 package trabexceção;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main{
     static void jogar(Caverna cav){
+        Random jv = new Random();
+        int rand;
         cav.iniciar();
-    }    
-    public static void main(String[] args){
-	Scanner scan = new Scanner(System.in);
-        //menu inicial
-        System.out.println("Digite o tamanho da câmara: (Mínimo: 5)");
-        int tamanho = scan.nextInt();
-        while(tamanho < 5){
-            System.out.println("Digite um valor maior ou igual a 5.");
-            tamanho = scan.nextInt();
+        cav.visualizarCenario();
+        TipoCamara tipo = cav.getCamara().tipo();
+        if(tipo == TipoCamara.ERRADA){
+            while(!(cav.getAventureiro().encontrouMensagem(cav.getCamara()))){
+                rand = jv.nextInt(4) + 1;
+                switch(rand){
+                    case 1:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.UP);
+                        System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    case 2:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.DOWN);
+                            System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    case 3:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.LEFT);
+                            System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    case 4:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.RIGHT);
+                            System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
+    }    
+    public static void main(String[] args) throws AcaoInvalidaException{
         Aventureiro mv = new Aventureiro("Marcos Vinicius");
-        Caverna cav = new Caverna(mv, tamanho);
+        Caverna cav = new Caverna(mv, 7);
         jogar(cav);
     }
 }
