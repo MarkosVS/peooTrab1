feb89fc96444397b028f573d470072f42fff9d28
Marcos Sombra <markos.vs@live.com>
2017-06-15 15:50:35 -0300

    metodo jogar almost concluido

--- Main.java
+++ Main.java
@@ -7,7 +7,6 @@ public class Main{
         Random jv = new Random();
         int rand;
         cav.iniciar();
-        cav.visualizarCenario();
         TipoCamara tipo = cav.getCamara().tipo();
         if(tipo == TipoCamara.ERRADA){
             while(!(cav.getAventureiro().encontrouMensagem(cav.getCamara()))){
@@ -53,6 +52,101 @@ public class Main{
                         break;
                 }
             }
+            System.out.println("");
+            System.out.println("O tesouro está em outra câmara.");
+            System.out.println("Gerando nova câmara...");
+            System.out.println("");
+            jogar(cav);
+        }else if(tipo == TipoCamara.FACIL){
+            while(!(cav.getAventureiro().encontrouTesouro(cav.getCamara()))){
+                rand = jv.nextInt(4) + 1;
+                switch(rand){
+                    case 1:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.UP);
+                        System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    case 2:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.DOWN);
+                            System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    case 3:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.LEFT);
+                            System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    case 4:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.RIGHT);
+                            System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            System.out.println("Você ganhou");
+        }else{
+            while(!(cav.getAventureiro().encontrouTesouro(cav.getCamara()))){
+                rand = jv.nextInt(4) + 1;
+                switch(rand){
+                    case 1:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.UP);
+                        System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    case 2:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.DOWN);
+                            System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    case 3:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.LEFT);
+                            System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    case 4:
+                        try{
+                            cav.getAventureiro().mover(cav.getCamara(), Movimento.RIGHT);
+                            System.out.println("");
+                        }catch(AcaoInvalidaException e){
+                            System.out.println(e);
+                            System.out.println("");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            System.out.println("Você ganhou");
         }
     }    
     public static void main(String[] args) throws AcaoInvalidaException{
